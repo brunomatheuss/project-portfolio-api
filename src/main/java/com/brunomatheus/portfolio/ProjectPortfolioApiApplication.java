57aac1c9f69e59d8612861fd86ddb1457fd759a7
bruno <brunomatheuss203@gmail.com>
2026-04-09 16:11:12 -0300

    creating project

---
+++ src/main/java/com/brunomatheus/portfolio/ProjectPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.brunomatheus.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPortfolioApiApplication.class, args);
+	}
+
+}
